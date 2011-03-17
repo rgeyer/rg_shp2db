3fc7fd707b0b5a4dacd81f7d18afdbf6d616d386
Ryan Geyer <me@ryangeyer.com>
2011-03-17 16:50:06 -0700

    deault != default

--- chef/cookbooks/rg_shp2db/recipes/coordinator.rb
+++ chef/cookbooks/rg_shp2db/recipes/coordinator.rb
@@ -11,7 +11,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-include_recipe "rubygems::deault"
+include_recipe "rubygems::default"
 
 package "shapelib"
 
